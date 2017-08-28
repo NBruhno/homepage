4bddeec1fb259d042314a0d401f470378c079b8a
Nicolai Bruhn <mail@bruhno.com>
2017-08-28 12:43:52 +0200

    Most recent update

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+    scrollToContact() {
+        const el = document.getElementById('contactTarget');
+        el.scrollIntoView({behavior: 'smooth'});
+    }
+
+    scrollToAbout() {
+        const el = document.getElementById('aboutTarget');
+        el.scrollIntoView({behavior: 'smooth'});
+    }
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
