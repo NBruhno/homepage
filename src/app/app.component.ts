0d311080aa8d8c9f7431845441a8c0f9384a73f2
Bruhno <mail@bruhno.com>
2017-08-29 02:38:10 +0200

    Adding future support for authentication.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app';
+    user: Observable<firebase.User>;
+    items: FirebaseListObservable<any[]>;
+    msgVal = '';
+
+    constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+        this.items = af.list('/messages', {
+            query: {
+                limitToLast: 50
+            }
+        });
+        this.user = this.afAuth.authState;
+    }
+
+    login() {
+        this.afAuth.auth.signInAnonymously();
+    }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
+
+    Send(desc: string) {
+        this.items.push({ message: desc});
+        this.msgVal = '';
+    }
 }
