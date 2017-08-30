f27f66aa912a424770a6c24b571545d19f14631d
Bruhno <mail@bruhno.com>
2017-08-30 16:15:21 +0200

    Beginning implementation of the authentication system using Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,8 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    user: Observable<firebase.User>;
-    items: FirebaseListObservable<any[]>;
-    msgVal = '';
-
-    constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-        this.items = af.list('/messages', {
-            query: {
-                limitToLast: 50
-            }
-        });
-        this.user = this.afAuth.authState;
-    }
-
-    login() {
-        this.afAuth.auth.signInAnonymously();
-    }
-
-    logout() {
-        this.afAuth.auth.signOut();
-    }
-
-    Send(desc: string) {
-        this.items.push({ message: desc});
-        this.msgVal = '';
-    }
-}
+export class AppComponent {}
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    email: string;
+    password: string;
 
-  ngOnInit() {
-  }
+    constructor(public authService: AuthService) {}
+
+    signup() {
+        this.authService.signup(this.email, this.password);
+        this.email = this.password = '';
+    }
+
+    login() {
+        this.authService.login(this.email, this.password);
+        this.email = this.password = '';
+    }
+
+    logout() {
+        this.authService.logout();
+    }
+
+    ngOnInit() {
+    }
 
 }
