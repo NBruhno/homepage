8f6505d69775a20905a66ad6e60c25c9341f3eaf
Bruhno <mail@bruhno.com>
2017-09-03 22:27:41 +0200

    Further development on authorization

---
+++ src/app/front/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+    ngOnInit() { }
+
+}
