cc037919246d399189343943fa21e920f657c277
Bruhno <mail@bruhno.com>
2017-09-14 17:16:52 +0200

    Reordering of files and removal of unused components

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../auth.service';
+import { AuthService } from '../auth.service';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
