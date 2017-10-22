ae5c6fd516a388afe4bad0ff88743da3a3fc0fc8
Bruhno <mail@bruhno.com>
2017-10-22 17:39:25 +0200

    Including the new dialog service

---
+++ src/app/dialog.service.ts
@@ -0,0 +1,45 @@
+import { Component, Injectable, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { UsernameComponent } from "./authentication/username/username.component";
+
+@Injectable()
+export class DialogService {
+
+    constructor(public dialog: MatDialog) {}
+
+    openDialog(message: string, type: string): void {
+        if (type === 'error') {
+            let dialogRef = this.dialog.open(AlertDialog, {
+                data: { message: message }
+            });
+
+            dialogRef.afterClosed().subscribe(result => {
+            });
+        }
+
+        if (type === 'username') {
+            let dialogRef = this.dialog.open(UsernameComponent, {
+                data: { message: message }
+            });
+
+            dialogRef.afterClosed().subscribe(result => {
+            });
+        }
+    }
+}
+
+@Component({
+    selector: 'alert-dialog',
+    template: '<span mat-dialog-title>An error has occurred</span><p class="mat-body">{{data.message}}</p><button mat-button (click)="onNoClick();">Dismiss</button>',
+})
+export class AlertDialog {
+
+    constructor(
+        public dialogRef: MatDialogRef<AlertDialog>,
+        @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+}
