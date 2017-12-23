d54d5eda45bd4dd4b92c611d9e73e80d0b7ba4a3
Bruhno <mail@bruhno.com>
2017-12-23 12:55:41 +0100

    Small change in the environment to account for changes in prod as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -25,7 +25,7 @@ export const environment = {
         { path: 'login', component: AuthenticationComponent, data: {title: 'Login • Bruhno'} },
         { path: 'gallery', component: NotFoundComponent, data: {title: 'Gallery • Bruhno'} },
         { path: 'projects', component: ProjectsComponent, data: {title: 'Projects • Bruhno'} },
-        { path: 'projects/projectID', component: ProjectTemplateComponent, data: {title: 'Project • Bruhno'} },
+        { path: 'projects/:projectID', component: ProjectTemplateComponent, data: {title: 'Project • Bruhno'} },
         { path: 'profile', component: ProfileComponent, data: {title: 'Profile • Bruhno'}, canActivate: [AuthGuard] },
         { path: 'upload', component: UploadComponent, data: {title: 'Upload • Bruhno'}, canActivate: [AuthGuard] },
         { path: '404', component: NotFoundComponent, data: {title: 'Not found (404)'} },
