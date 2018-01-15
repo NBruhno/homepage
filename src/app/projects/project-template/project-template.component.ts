53f28565a7a20aa8f5fe6a018d02c056240ae551
Bruhno <mail@bruhno.com>
2018-01-16 00:08:55 +0100

    Projects are now fully implemented. Some more well written than others based on how long since I completed them. Also moved some assets to another folder on my host.

--- src/app/projects/project-template/project-template.component.ts
+++ src/app/projects/project-template/project-template.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IDService } from '../../id.service';
 import { NgxImageGalleryComponent, GALLERY_IMAGE, GALLERY_CONF } from 'ngx-image-gallery';
+import { DomSanitizer } from '@angular/platform-browser';
+import { EmbedVideoService } from 'ngx-embed-video/dist';
 
 @Component({
     selector: 'app-project-template',
@@ -15,6 +17,15 @@ export class ProjectTemplateComponent implements OnInit {
     @ViewChild('galleryIP') galleryIP: NgxImageGalleryComponent;
     currentProjects = ['qfd-2017', 'bwf-2017', 'derailed-2016', 'drummute-2016', 'image-processing-2015'];
     idExists = false;
+    bwfVid: any;
+    derailedVid: any;
+    drummuteVid: any;
+    ipVid: any;
+    youtubeVidOptions = {
+        query: { portrait: 0, color: '333' },
+        attr: { width: 762, height: 430 },
+        image: 'maxresdefault'
+    };
 
     conf: GALLERY_CONF = {
         imageOffset: '0px',
@@ -32,51 +43,42 @@ export class ProjectTemplateComponent implements OnInit {
     };
 
     imagesQFD: GALLERY_IMAGE[] = [
-        { url: '../../../assets/images/logo_resized.png', },
-        { url: '../../../assets/images/qfd-screencap1.png', },
-        { url: '../../../assets/images/qfd-screencap2.png', },
-        { url: '../../../assets/images/qfd-screencap3.png', },
-        { url: '../../../assets/images/qfd-screencap4.png', },
-        { url: '../../../assets/images/qfd-screencap5.png', },
+        { url: 'https://img.bruhno.com/projects/QFDCenterLogo.png' },
+        { url: 'https://img.bruhno.com/projects/qfd-screencap1.png' },
+        { url: 'https://img.bruhno.com/projects/qfd-screencap2.png' },
+        { url: 'https://img.bruhno.com/projects/qfd-screencap3.png' },
+        { url: 'https://img.bruhno.com/projects/qfd-screencap4.png' },
+        { url: 'https://img.bruhno.com/projects/qfd-screencap5.png' }
     ];
 
     imagesBWF: GALLERY_IMAGE[] = [
-        { url: '../../../assets/images/logo_resized.png', },
-        { url: '../../../assets/images/qfd-screencap1.png', },
-        { url: '../../../assets/images/qfd-screencap2.png', },
-        { url: '../../../assets/images/qfd-screencap3.png', },
-        { url: '../../../assets/images/qfd-screencap4.png', },
-        { url: '../../../assets/images/qfd-screencap5.png', },
+        { url: 'https://img.bruhno.com/projects/BWF Logo.png' },
+        { url: 'https://img.bruhno.com/projects/BWFJacketAndTheEnvironment.png' },
+        { url: 'https://img.bruhno.com/projects/Hi-Fi.png' },
+        { url: 'https://img.bruhno.com/projects/phonePage2.png' }
     ];
 
     imagesDerail: GALLERY_IMAGE[] = [
-        { url: '../../../assets/images/logo_resized.png', },
-        { url: '../../../assets/images/qfd-screencap1.png', },
-        { url: '../../../assets/images/qfd-screencap2.png', },
-        { url: '../../../assets/images/qfd-screencap3.png', },
-        { url: '../../../assets/images/qfd-screencap4.png', },
-        { url: '../../../assets/images/qfd-screencap5.png', },
+        { url: 'https://img.bruhno.com/projects/PerceptionNeuron.png' },
+        { url: 'https://img.bruhno.com/projects/readycheck.PNG' }
     ];
 
     imagesDrum: GALLERY_IMAGE[] = [
-        { url: '../../../assets/images/logo_resized.png', },
-        { url: '../../../assets/images/qfd-screencap1.png', },
-        { url: '../../../assets/images/qfd-screencap2.png', },
-        { url: '../../../assets/images/qfd-screencap3.png', },
-        { url: '../../../assets/images/qfd-screencap4.png', },
-        { url: '../../../assets/images/qfd-screencap5.png', },
+        { url: 'https://img.bruhno.com/projects/circuitLayout.png' },
+        { url: 'https://img.bruhno.com/projects/sensorLayout.png' }
     ];
 
     imagesIP: GALLERY_IMAGE[] = [
-        { url: '../../../assets/images/logo_resized.png', },
-        { url: '../../../assets/images/qfd-screencap1.png', },
-        { url: '../../../assets/images/qfd-screencap2.png', },
-        { url: '../../../assets/images/qfd-screencap3.png', },
-        { url: '../../../assets/images/qfd-screencap4.png', },
-        { url: '../../../assets/images/qfd-screencap5.png', },
+        { url: 'https://img.bruhno.com/projects/Defect.png' },
+        { url: 'https://img.bruhno.com/projects/Screenshot_1.png' }
     ];
 
-    constructor(public id: IDService) {}
+    constructor(public id: IDService, private embedService: EmbedVideoService) {
+        this.bwfVid = this.embedService.embed_youtube('jFgNkVrv_bQ', this.youtubeVidOptions);
+        this.derailedVid = this.embedService.embed_youtube('SBas-H8hG_A', this.youtubeVidOptions);
+        this.drummuteVid = this.embedService.embed_youtube('0hjBSHSQfAM', this.youtubeVidOptions);
+        this.ipVid = this.embedService.embed_youtube('eH4c7C6v9Z4', this.youtubeVidOptions);
+    }
 
     ngOnInit() {
         for (let i = 0; i < this.currentProjects.length; i++) {
@@ -85,5 +87,4 @@ export class ProjectTemplateComponent implements OnInit {
             }
         }
     }
-
 }
