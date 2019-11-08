603c2bdf0273e2180bfa658673a0448e28a6ea0e
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-11-08 18:38:21 +0100

    Cleared folder

--- src/vendor/main.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This is an entry point for additional assets, require your assets under this file.
- * Example:
- *   require('./bootstrap/css/bootstrap.min.css');
- * If you try using bootstrap 4, see: https://github.com/shakacode/bootstrap-loader/issues/244
- */
