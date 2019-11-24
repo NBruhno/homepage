751fcda8e73e5c93259b4b1af14d15f1dfa1bc2a
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-11-24 17:44:54 +0100

    Corrected comma in the sitemap

--- src/scripts/createSitemap.js
+++ src/scripts/createSitemap.js
@@ -56,7 +56,7 @@ domains.forEach((domain) => {
 
 fs.writeFileSync(`${outputLocation}/sitemap.xml`, `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	${sitemapEntries}
+	${sitemapEntries.join('\n	')}
 </urlset>`)
 
 console.log(`Created sitemap (${outputLocation}/sitemap.xml)`)
