1d98b0ed141c66502527b9917947072270c57661
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-12-06 13:05:42 +0100

    Upgraded dependencies

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
+
+import Page from 'components/Page'
 
 const Projects: NextPage = () => (
-	<div>
-		This is a placeholder
-	</div>
+	<>
+		<Head>
+			<title>Bruhno</title>
+		</Head>
+		<Page>
+			<div>
+				This is a placeholder
+			</div>
+		</Page>
+	</>
 )
 
 export default Projects
