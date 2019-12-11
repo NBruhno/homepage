b060dd79e53e21ed345bb2dd8c8bd4228744a783
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-12-11 22:50:13 +0100

    Added redux devtools support using reinspect and cleaned up types

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,18 +2,24 @@ import { NextPage } from 'next'
 import Head from 'next/head'
 
 import Page from 'components/Page'
+import useCounter from 'reducers/useCounter'
 
-const Projects: NextPage = () => (
-	<>
-		<Head>
-			<title>Bruhno</title>
-		</Head>
-		<Page>
-			<div>
-				This is a placeholder
-			</div>
-		</Page>
-	</>
-)
+const Projects: NextPage = () => {
+	const { count } = useCounter()
+
+	return (
+		<>
+			<Head>
+				<title>Bruhno</title>
+			</Head>
+			<Page>
+				<div>
+					This is a placeholder
+					<p>State count: {count}</p>
+				</div>
+			</Page>
+		</>
+	)
+}
 
 export default Projects
--- src/reducers/useCounter.ts
+++ src/reducers/useCounter.ts
@@ -1,13 +1,19 @@
 import { useStore } from 'lib/store'
 
+export const ACTIONS = {
+	INCREMENT: 'INCREMENT',
+	DECREMENT: 'DECREMENT',
+	RESET: 'RESET',
+}
+
 const useCounter = () => {
 	const { state, dispatch } = useStore()
 	return {
 		count: state.count,
 		message: state.message,
-		increment: () => dispatch({ type: 'increment', message: 'Incremented' }),
-		decrement: () => dispatch({ type: 'decrement', message: 'Decremented' }),
-		reset: () => dispatch({ type: 'reset', message: 'Reset' }),
+		increment: () => dispatch({ type: ACTIONS.INCREMENT, message: 'Incremented', payload: { count: state.count + 1, message: 'Incremented' } }),
+		decrement: () => dispatch({ type: ACTIONS.DECREMENT, message: 'Decremented', payload: { count: state.count - 1, message: 'Decremented' } }),
+		reset: () => dispatch({ type: ACTIONS.RESET, message: 'Reset', payload: { count: 0, message: 'Reset' } }),
 	}
 }
 
