671995707cfdd8bf34a6886636e64f4bc2cbed7e
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-12-15 19:36:05 +0100

    Firebase moved to its own bundle and added api example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
+import fetch from 'isomorphic-unfetch'
 import { NextPage } from 'next'
 import Link from 'next/link'
 import Head from 'next/head'
+
 import Page from 'components/Page'
-
 import useCounter from 'reducers/useCounter'
-// import useTestList from 'reducers/useTestList'
-// import useTest from 'reducers/useTest'
 
-const Home: NextPage<{ userAgent?: string }> = ({ userAgent }) => {
+const Home: NextPage<{ userAgent?: string, test: [{ title: string }] }> = ({ userAgent, test }) => {
 	const { count, message, increment, decrement, reset } = useCounter()
-	// const [test, testLoading, testError] = useTest('hC80Jz6cY0PVFCcTRHF8')
-	// const { tests, error, loading } = useTestList()
-
-	// if (!loading) {
-	// 	console.log(tests || error)
-	// }
 
 	return (
 		<>
@@ -26,19 +19,20 @@ const Home: NextPage<{ userAgent?: string }> = ({ userAgent }) => {
 				<Link href='/projects'>
 					<a>Projects</a>
 				</Link>
+				{test && test.map(({ title }, index) => <div key={index}>{title}</div>)}
 				<p>Global state count: {count}</p>
 				{message && <p>Message: <b>{message} to {count}</b></p>}
 				<button onClick={() => increment()} type='button'>+1</button>
 				<button onClick={() => decrement()} type='button'>-1</button>
 				<button onClick={() => reset()} type='button'>reset</button>
-				{/* {testLoading && <p>Loading...</p>}
-				{testError && <p>Error {testError}</p>}
-				{test && <p>{test?.title}</p>} */}
 			</Page>
 		</>
 	)
 }
 
-Home.getInitialProps = async ({ req }) => ({ userAgent: req ? req.headers['user-agent'] || '' : navigator.userAgent })
+Home.getInitialProps = async ({ req }) => {
+	const response = await fetch('http://localhost:3000/api/test')
+	return { test: await response.json(), userAgent: req ? req.headers['user-agent'] || '' : navigator.userAgent }
+}
 
 export default Home
--- src/reducers/useTest.ts
+++ src/reducers/useTest.ts
@@ -1,29 +1,40 @@
-import { useEffect, useCallback } from 'react'
+import { useEffect, useCallback, useState } from 'react'
 import { useStore } from 'lib/store'
 import { useDocument } from 'react-firebase-hooks/firestore'
 
-import firebase from 'lib/firebase'
+import firebasePromise from 'lib/firebase'
 
 export const ACTIONS = {
 	GET_TESTS: 'GET_TESTS',
 }
 
-const useTests = (testId: string) => {
+const useTest = (testId: string) => {
 	const { state, dispatch } = useStore()
-	const [snapshot, loading, error] = useDocument(firebase.firestore().doc(`test/${testId}`))
+	const [firebase, setFirebase] = useState(null)
+	const [snapshot, loading, error] = useDocument(firebase?.firestore()?.doc(`test/${testId}`))
 
-	const dispatchToGlobalState = useCallback(async () => dispatch({
+	const dispatchToGlobalState = useCallback(() => dispatch({
 		type: ACTIONS.GET_TESTS,
-		payload: { test: { data: snapshot.data(), loading, error } },
+		payload: { test: { data: snapshot?.data(), loading, error } },
 	}), [dispatch, error, loading, snapshot])
 
 	useEffect(() => {
-		if (!loading && state.test.data?.updatedAt !== snapshot.data().updatedAt) {
+		if (!firebase) {
+			firebasePromise().then((value) => {
+				setFirebase(value)
+			})
+		}
+
+		if (!loading && state.test?.data?.updatedAt !== snapshot?.data()?.updatedAt) {
 			dispatchToGlobalState()
 		}
-	}, [dispatchToGlobalState, loading, state.test.data, snapshot])
+	}, [dispatchToGlobalState, loading, state.test, state.test.data, snapshot, firebase])
 
-	return [state.test.data, state.test.loading, state.test.error]
+	return [
+		state.test.data,
+		state.test.loading,
+		state.test.error,
+	]
 }
 
-export default useTests
+export default useTest
