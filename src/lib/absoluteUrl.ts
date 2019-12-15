53d38fd0fc9b85356246fe68d6b5ee87329c04c0
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2019-12-15 19:59:38 +0100

    Corrected issue of getting a proper origin URL for the serverless API calls

---
+++ src/lib/absoluteUrl.ts
@@ -0,0 +1,15 @@
+const absoluteUrl = (req: any) => {
+	let protocol = 'https:'
+	const host = req ? (req.headers['x-forwarded-host'] || req.headers['host']) : window.location.host
+	if (host.indexOf('localhost') > -1) {
+		protocol = 'http:'
+	}
+
+	return {
+		protocol,
+		host,
+		origin: protocol + '//' + host,
+	}
+}
+
+export default absoluteUrl
