ca46d1cb389ce33515d84fc9cb63f444b8f7c54e
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-06-02 18:23:49 +0200

    Added the env variables to the config

--- next.config.js
+++ next.config.js
@@ -35,6 +35,22 @@ module.exports = withBundleAnalyzer(withOffline(withSourceMaps(withTranspileModu
 		],
 	},
 
+	env: {
+		FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+		FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+		FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+		FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+		FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+		FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+		FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+		FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+		FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+		FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+		SENTRY_DSN: process.env.SENTRY_DSN,
+		ANALYZE_BUILD: process.env.ANALYZE_BUILD,
+		ENVIRONMENT: process.env.ENVIRONMENT,
+	},
+
 	webpack: (config) => {
 		// Fixes npm packages that depend on `fs` module
 		config.node = { fs: 'empty' }
