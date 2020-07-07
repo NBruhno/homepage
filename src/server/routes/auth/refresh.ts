26171d7e4d9f6f9ac5b106b035e5af5a2815c29b
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-07-07 22:45:46 +0200

    Resolve issue with refresh

--- src/server/routes/auth/refresh.ts
+++ src/server/routes/auth/refresh.ts
@@ -1,25 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { config } from 'config.server'
+
 import { authenticateRefreshToken, setRefreshCookie } from 'server/middleware'
 import { generateAccessToken, generateRefreshToken } from 'server/generateTokens'
 
 export const refresh = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { method, cookies } = req
+	const refreshToken = config.environment !== 'development' ? cookies['__Host-refreshToken'] : cookies['refreshToken']
 
 	switch (method) {
 		case 'GET': {
 			try {
-				if (!cookies.refreshToken) {
+				if (!refreshToken) {
 					res.status(400).end('Invalid token.')
 					break
 				}
 
-				const token: Record<string, any> = await authenticateRefreshToken(req, res, cookies.refreshToken)
+				const token: Record<string, any> = await authenticateRefreshToken(req, res, refreshToken)
 
 				const accessToken = generateAccessToken(token.secret, { email: token.email, sub: token.id })
-				const refreshToken = generateRefreshToken(token.secret, { email: token.email, sub: token.id })
+				const newRefreshToken = generateRefreshToken(token.secret, { email: token.email, sub: token.id })
 
-				setRefreshCookie(res, refreshToken)
+				setRefreshCookie(res, newRefreshToken)
 
 				res.setHeader('Content-Type', 'text/plain')
 				res.status(200).send(accessToken)
