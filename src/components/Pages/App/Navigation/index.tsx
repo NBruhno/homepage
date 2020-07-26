ee06ebe6e172c8946f82f0f4653ba4e4868cde6d
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-07-26 19:35:17 +0200

    Fixed linter error

--- src/components/Pages/App/Navigation/index.tsx
+++ src/components/Pages/App/Navigation/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { config } from 'config.client'
-
 import { useRefresh } from 'reducers/refresh'
 import { useAuth } from 'reducers/auth'
 
