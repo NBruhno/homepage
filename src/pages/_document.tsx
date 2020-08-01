d6bbf4baf69b9ab89da69c529285c22fe466ec2d
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-08-01 23:06:02 +0200

    Added a few optimal changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
 				<Head>
 					{/* General */}
 					<base href='/' />
+					<meta charSet='UTF-8' />
 					<meta name='description' content="This is Nicolai Bruhn Lauritsen or more commonly known as Bruhno's personal website which functions as a portfolio and a playground." />
 					<meta name='mobile-web-app-capable' content='yes' />
 					<meta name='apple-mobile-web-app-capable' content='yes' />
