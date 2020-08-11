cf65b260df2bded1ee136c8902ed62ea0d925b77
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-08-11 15:09:41 +0200

    Updated simple dependencies

--- src/components/Pages/App/Footer/index.tsx
+++ src/components/Pages/App/Footer/index.tsx
@@ -5,7 +5,11 @@ export const Footer = (props: React.ComponentProps<'footer'>) => (
 	<Container {...props}>
 		<Link href='mailto: mail@bruhno.com'>Email</Link>
 		<span css={{ opacity: 0.7 }}> • </span>
-		<Link href='https://www.linkedin.com/in/bruhno/'>LinkedIn</Link>
+		<Link href='https://www.linkedin.com/in/bruhno'>LinkedIn</Link>
+		<span css={{ opacity: 0.7 }}> • </span>
+		<Link href='https://github.com/NBruhno'>GitHub</Link>
+		<span css={{ opacity: 0.7 }}> • </span>
+		<Link href='https://gitlab.com/Bruhno'>GitLab</Link>
 		<div css={{ opacity: 0.7 }}>
 			<small>
 				© 2010-{new Date().toLocaleString('en-DK', { year: 'numeric' })} Bruhno, All rights reserved.
