73056dca5c11ea50b69eaeaa847dfd72a8e54cb5
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-08-16 20:55:14 +0200

    Cleaned up the tooltip

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,6 @@
 export default {
 	stories: ['../src/**/*.stories.@(js|ts|tsx|mdx)'],
 	addons: [
-		'@storybook/addon-a11y',
 		'@storybook/addon-backgrounds',
 		'@storybook/addon-docs',
 		'@storybook/addon-toolbars',
