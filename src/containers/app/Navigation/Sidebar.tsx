ec284e0ec261bbe2a4b4747d5724970e66753844
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-08-23 22:44:48 +0200

    Moved sidebar hiding contstraints to css media queries

--- src/containers/app/Navigation/Sidebar.tsx
+++ src/containers/app/Navigation/Sidebar.tsx
@@ -10,11 +10,13 @@ const defaultCss = (theme: Theme, collapsed: boolean) => css({
 	alignItems: 'stretch',
 	backgroundColor: theme.color.background,
 	borderRight: `1px solid ${theme.color.border}`,
-	display: 'flex',
 	flexDirection: 'column',
 	justifyContent: 'space-between',
 	padding: '0',
+	opacity: 0,
+	visibility: 'hidden',
 	zIndex: 10,
+	display: 'none',
 
 	'> ::-webkit-scrollbar': {
 		width: collapsed ? 0 : '8px',
@@ -29,16 +31,20 @@ export const DesktopSidebar = ({ collapsed, ...rest }: Props) => {
 			css={[
 				defaultCss(theme, collapsed),
 				{
-					height: '100vh',
-					position: 'sticky',
-					top: 0,
-					transform: 'none',
-					transition: `width 300ms ${theme.animation.default}`,
-					width: collapsed ? '70px' : '250px',
+					[theme.mediaQueries.minMobile]: {
+						display: 'flex',
+						height: '100vh',
+						opacity: 1,
+						position: 'sticky',
+						top: 0,
+						transition: `width 300ms ${theme.animation.default}`,
+						visibility: 'visible',
+						width: collapsed ? '70px' : '250px',
 
-					'@supports ((-webkit-backdrop-filter: blur(8px)) or (backdrop-filter: blur(8px)))': {
-						backdropFilter: 'none',
-						backgroundColor: transparentize(1, theme.color.background),
+						'@supports ((-webkit-backdrop-filter: blur(8px)) or (backdrop-filter: blur(8px)))': {
+							backdropFilter: 'none',
+							backgroundColor: transparentize(1, theme.color.background),
+						},
 					},
 				},
 			]}
@@ -55,18 +61,21 @@ export const MobileSidebar = ({ show, ...rest }: Props & { show: boolean }) => {
 			css={[
 				defaultCss(theme, !show),
 				{
-					height: 'calc(100vh - 53px)',
-					position: 'fixed',
-					top: '54px',
-					opacity: show ? 1 : 0,
-					visibility: show ? 'visible' : 'hidden',
-					transform: show ? 'none' : 'translate(-251px)',
-					transition: `transform 300ms ${theme.animation.default}`,
-					width: '250px',
+					[theme.mediaQueries.maxMobile]: {
+						display: 'flex',
+						height: 'calc(100vh - 53px)',
+						opacity: show ? 1 : 0,
+						position: 'fixed',
+						top: '54px',
+						transform: show ? 'none' : 'translate(-251px)',
+						transition: `transform 300ms ${theme.animation.default}`,
+						visibility: show ? 'visible' : 'hidden',
+						width: '250px',
 
-					'@supports ((-webkit-backdrop-filter: blur(8px)) or (backdrop-filter: blur(8px)))': {
-						backdropFilter: 'saturate(150%) blur(5px)',
-						backgroundColor: transparentize(0.2, theme.color.background),
+						'@supports ((-webkit-backdrop-filter: blur(8px)) or (backdrop-filter: blur(8px)))': {
+							backdropFilter: 'saturate(150%) blur(5px)',
+							backgroundColor: transparentize(0.2, theme.color.background),
+						},
 					},
 				},
 			]}
--- src/containers/app/Navigation/index.tsx
+++ src/containers/app/Navigation/index.tsx
@@ -27,8 +27,8 @@ export const Navigation = () => {
 		if (isMobile) updateResponsive({ showMenu: false })
 	}
 
-	if (!isMobile) {
-		return (
+	return (
+		<>
 			<DesktopSidebar collapsed={collapsedSidebar}>
 				<Link href='/' passHref>
 					<Header onClick={() => closeMenuOnInteraction()}>
@@ -162,119 +162,116 @@ export const Navigation = () => {
 					/>
 				</Content>
 			</DesktopSidebar>
-		)
-	}
-
-	return (
-		<MobileSidebar show={showMenu}>
-			<Content css={{ paddingTop: '12px' }}>
-				<Link href='/users/profile' passHref>
-					<NavLink
-						active={pathname.includes('/users/profile')}
-						onClick={() => closeMenuOnInteraction()}
-					>
-						<AccountIcon css={{ marginRight: '6px' }} size={22} />
-						<Placeholder isLoading={!user.isStateKnown}>
-							{user.accessToken ? (
-								<Text>
-									{user.displayName}
-								</Text>
-							) : (
-								<Text>Not logged in</Text>
+			<MobileSidebar show={showMenu}>
+				<Content css={{ paddingTop: '12px' }}>
+					<Link href='/users/profile' passHref>
+						<NavLink
+							active={pathname.includes('/users/profile')}
+							onClick={() => closeMenuOnInteraction()}
+						>
+							<AccountIcon css={{ marginRight: '6px' }} size={22} />
+							<Placeholder isLoading={!user.isStateKnown}>
+								{user.accessToken ? (
+									<Text>
+										{user.displayName}
+									</Text>
+								) : (
+									<Text>Not logged in</Text>
+								)}
+							</Placeholder>
+						</NavLink>
+					</Link>
+					{user.accessToken ? (
+						<ButtonText
+							css={{ margin: '4px 12px', height: '35px' }}
+							slim
+							label={(
+								<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
+									<LogoutIcon css={{ marginRight: '6px' }} />Logout
+								</div>
 							)}
-						</Placeholder>
-					</NavLink>
-				</Link>
-				{user.accessToken ? (
-					<ButtonText
-						css={{ margin: '4px 12px', height: '35px' }}
-						slim
-						label={(
-							<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
-								<LogoutIcon css={{ marginRight: '6px' }} />Logout
-							</div>
-						)}
-						onClick={async () => { await logout() }}
-					/>
-				) : (
-					<ButtonText
-						css={{ margin: '4px 12px', height: '35px' }}
-						slim
-						label={(
-							<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
-								<LoginIcon css={{ marginRight: '6px' }} />Login
-							</div>
-						)}
-						onClick={() => {
-							window.scrollTo(0, 0)
-							if (isMobile) {
-								updateResponsive({ showLogin: !showLogin, showMenu: false })
-							} else {
-								updateResponsive({ showLogin: !showLogin })
-							}
-						}}
-					/>
-				)}
-				<Separator collapsed={false}>Tools</Separator>
-				<Link href='/games' passHref>
-					<NavLink
-						active={pathname.includes('/games')}
-						onClick={() => closeMenuOnInteraction()}
-					>
-						<ControllerIcon css={{ marginRight: '6px' }} size={22} /><Text>Games</Text>
-					</NavLink>
-				</Link>
+							onClick={async () => { await logout() }}
+						/>
+					) : (
+						<ButtonText
+							css={{ margin: '4px 12px', height: '35px' }}
+							slim
+							label={(
+								<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
+									<LoginIcon css={{ marginRight: '6px' }} />Login
+								</div>
+							)}
+							onClick={() => {
+								window.scrollTo(0, 0)
+								if (isMobile) {
+									updateResponsive({ showLogin: !showLogin, showMenu: false })
+								} else {
+									updateResponsive({ showLogin: !showLogin })
+								}
+							}}
+						/>
+					)}
+					<Separator collapsed={false}>Tools</Separator>
+					<Link href='/games' passHref>
+						<NavLink
+							active={pathname.includes('/games')}
+							onClick={() => closeMenuOnInteraction()}
+						>
+							<ControllerIcon css={{ marginRight: '6px' }} size={22} /><Text>Games</Text>
+						</NavLink>
+					</Link>
+					<Link href='/404' passHref>
+						<NavLink
+							active={pathname.includes('/test')}
+							onClick={() => closeMenuOnInteraction()}
+						>
+							<TestTubeIcon css={{ marginRight: '6px' }} size={22} /><Text>Test</Text>
+						</NavLink>
+					</Link>
+					<Link href='/storybook' passHref>
+						<NavLink
+							active={pathname.includes('/storybook')}
+							onClick={() => closeMenuOnInteraction()}
+						>
+							<PencilRulerIcon css={{ marginRight: '6px' }} size={22} /><Text>Storybook</Text>
+						</NavLink>
+					</Link>
+				</Content>
+
+				<Separator collapsed={false}>Site</Separator>
 				<Link href='/404' passHref>
 					<NavLink
-						active={pathname.includes('/test')}
+						active={pathname.includes('/projects')}
 						onClick={() => closeMenuOnInteraction()}
 					>
-						<TestTubeIcon css={{ marginRight: '6px' }} size={22} /><Text>Test</Text>
+						<ToolsIcon css={{ marginRight: '6px' }} size={22} /><Text>Projects</Text>
 					</NavLink>
 				</Link>
-				<Link href='/storybook' passHref>
+				<Link href='/about' passHref>
 					<NavLink
-						active={pathname.includes('/storybook')}
+						active={pathname.includes('/about')}
 						onClick={() => closeMenuOnInteraction()}
 					>
-						<PencilRulerIcon css={{ marginRight: '6px' }} size={22} /><Text>Storybook</Text>
+						<InfoIcon css={{ marginRight: '6px' }} size={22} /><Text>About</Text>
 					</NavLink>
 				</Link>
-			</Content>
 
-			<Separator collapsed={false}>Site</Separator>
-			<Link href='/404' passHref>
-				<NavLink
-					active={pathname.includes('/projects')}
-					onClick={() => closeMenuOnInteraction()}
-				>
-					<ToolsIcon css={{ marginRight: '6px' }} size={22} /><Text>Projects</Text>
-				</NavLink>
-			</Link>
-			<Link href='/about' passHref>
-				<NavLink
-					active={pathname.includes('/about')}
-					onClick={() => closeMenuOnInteraction()}
-				>
-					<InfoIcon css={{ marginRight: '6px' }} size={22} /><Text>About</Text>
-				</NavLink>
-			</Link>
-
-			<Content css={{ marginTop: 'auto' }}>
-				<Separator slim />
-				<ButtonText
-					css={{ margin: '6px 12px 7px', height: '35px' }}
-					slim
-					label={(
-						<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
-							<LightDarkModeIcon css={{ marginRight: '6px' }} />
-							{darkTheme ? 'Light theme' : 'Dark theme'}
-						</div>
-					)}
-					onClick={() => updateResponsive({ darkTheme: !darkTheme })}
-				/>
-				<div css={(theme: Theme) => ({ borderTop: `1px solid ${theme.color.border}` })} />
-			</Content>
-		</MobileSidebar>
+				<Content css={{ marginTop: 'auto' }}>
+					<Separator slim />
+					<ButtonText
+						css={{ margin: '6px 12px 7px', height: '35px' }}
+						slim
+						label={(
+							<div css={{ display: 'flex', alignItems: 'center', height: '20px' }}>
+								<LightDarkModeIcon css={{ marginRight: '6px' }} />
+								{darkTheme ? 'Light theme' : 'Dark theme'}
+							</div>
+						)}
+						onClick={() => updateResponsive({ darkTheme: !darkTheme })}
+					/>
+					<div css={(theme: Theme) => ({ borderTop: `1px solid ${theme.color.border}` })} />
+				</Content>
+			</MobileSidebar>
+		</>
 	)
 }
