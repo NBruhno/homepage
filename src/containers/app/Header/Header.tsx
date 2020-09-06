5354c743403c25e90a079a80065f03bbe607f270
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-09-06 22:55:43 +0200

    Attempt to fix chrome mobile crash by removing scroll listener

--- src/containers/app/Header/Header.tsx
+++ src/containers/app/Header/Header.tsx
@@ -1,37 +1,31 @@
 import { transparentize } from 'polished'
 
-import { useScroll } from 'lib/useScroll'
+export const Header = (props: React.ComponentProps<'header'>) => (
+	<header
+		css={(theme: Theme) => ({
+			alignItems: 'center',
+			backdropFilter: 'saturate(150%) blur(5px)',
+			backgroundColor: theme.darkTheme ? theme.color.gray000 : theme.color.gray000,
+			boxShadow: !theme.darkTheme ? '0 2px 8px rgba(0, 0, 0, 0.15)' : 'none',
+			color: theme.darkTheme ? theme.color.text : theme.color.gray100,
+			display: 'flex',
+			height: '30px',
+			padding: '12px',
+			position: 'sticky',
+			top: 0,
+			transition: !theme.darkTheme ? `box-shadow 300ms ${theme.animation.default}` : 'none',
+			zIndex: 10,
 
-export const Header = (props: React.ComponentProps<'header'>) => {
-	const { scrollY } = useScroll()
-
-	return (
-		<header
-			css={(theme: Theme) => ({
-				alignItems: 'center',
+			'@supports ((-webkit-backdrop-filter: blur(5px)) or (backdrop-filter: blur(5px)))': {
 				backdropFilter: 'saturate(150%) blur(5px)',
-				backgroundColor: theme.darkTheme ? theme.color.gray000 : theme.color.gray000,
-				boxShadow: !theme.darkTheme && scrollY > 10 ? '0 2px 8px rgba(0, 0, 0, 0.15)' : 'none',
-				color: theme.darkTheme ? theme.color.text : theme.color.gray100,
-				display: 'flex',
-				height: '30px',
-				padding: '12px',
-				position: 'sticky',
-				top: 0,
-				transition: !theme.darkTheme && scrollY > 10 ? `box-shadow 300ms ${theme.animation.default}` : 'none',
-				zIndex: 10,
+				backgroundColor: transparentize(0.1, theme.color.gray000),
+			},
 
-				'@supports ((-webkit-backdrop-filter: blur(5px)) or (backdrop-filter: blur(5px)))': {
-					backdropFilter: 'saturate(150%) blur(5px)',
-					backgroundColor: transparentize(0.1, theme.color.gray000),
-				},
-
-				[theme.mediaQueries.minMobile]: {
-					display: 'none',
-					visibility: 'hidden',
-				},
-			})}
-			{...props}
-		/>
-	)
-}
+			[theme.mediaQueries.minMobile]: {
+				display: 'none',
+				visibility: 'hidden',
+			},
+		})}
+		{...props}
+	/>
+)
