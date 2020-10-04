b1cb61fb77a5866add078249c3a1292b9fe00efe
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-10-04 22:02:36 +0200

    Added a script that attempts to supply the Twitch API token on build

---
+++ src/scripts/createEnv.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+require('isomorphic-fetch')
+require('dotenv').config()
+
+const fs = require('fs')
+
+const { DEPLOY_ENV, IGDB_CLIENT_ID, IGDB_CLIENT_SECRET } = process.env
+
+const createEnv = async () => {
+	if (DEPLOY_ENV && DEPLOY_ENV !== 'local') {
+		try {
+			const token = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${IGDB_CLIENT_ID}&client_secret=${IGDB_CLIENT_SECRET}&grant_type=client_credentials`, {
+				method: 'POST',
+			})
+				.then(async (response) => {
+					const json = await response.json()
+					return json.access_token
+				})
+
+			fs.appendFile('.env', `IGDB_TOKEN="${token}"`, (error) => {
+				if (error) throw error
+				console.log('Deploy env appended to .env')
+			})
+		} catch (error) {
+			console.log(error)
+			throw new Error('Failed to generate deploy env')
+		}
+	} else console.log('Building locally, skipping deploy env generation')
+}
+
+createEnv()
