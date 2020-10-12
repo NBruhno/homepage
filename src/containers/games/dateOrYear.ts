29d8ae7263147fef1da8aff658aa4e40ef990319
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-10-12 21:50:26 +0200

    Corrected an issue with date calculation

--- src/containers/games/dateOrYear.ts
+++ src/containers/games/dateOrYear.ts
@@ -1,7 +1,7 @@
 import { lastDayOfYear, isSameDay } from 'date-fns'
 
 export const dateOrYear = (date: number | Date) => {
-	if (date === null) return 'TBA'
+	if (!date) return 'TBA'
 	if (isSameDay(lastDayOfYear(date), new Date(date))) {
 		return `TBA ${new Date(date).toLocaleString('en-DK', { year: 'numeric' })}`
 	}
