04b841515965380bd661ee952f1629a11d86058c
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2020-11-08 14:24:28 +0100

    Added trusted sources to CSP

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const basePath = ''
 const securityHeaders = [
 	{
 		key: 'Content-Security-Policy',
-		value: `default-src 'self'; img-src 'self' https: data:; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self'; frame-src ${NODE_ENV === 'development' ? 'localhost:9000' : 'self'}`,
+		value: `default-src 'self' ${NODE_ENV === 'development' && 'localhost:9000 localhost:3000'} https://*.bruhno.com https://*.bruhno.dev https://*.vercel-analytics.com; img-src 'self' https: data:; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self'`,
 	},
 	{
 		key: 'X-Frame-Options',
