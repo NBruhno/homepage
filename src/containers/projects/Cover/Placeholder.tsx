7891e1d1c353a150e9e72719c03250a999610f72
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2021-05-16 19:08:19 +0200

    Changed placeholder for project covers

--- src/containers/projects/Cover/Placeholder.tsx
+++ src/containers/projects/Cover/Placeholder.tsx
@@ -2,7 +2,7 @@ export const Placeholder = ({ ...rest }: React.ComponentProps<'div'>) => (
 	<div
 		css={(theme: Theme) => ({
 			flexShrink: 0,
-			backgroundColor: theme.color.grayDark,
+			background: [theme.color.inputBackgroundHover, `linear-gradient(134deg, ${theme.color.inputBorder} 0%, ${theme.color.inputBackgroundHover} 63%, ${theme.color.inputBackground} 100%)`],
 			height: '100%',
 			width: '100%',
 		})}
