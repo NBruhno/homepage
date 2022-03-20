3fac6419da169697449a0d44ba531111dd12c650
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-03-20 21:56:45 +0100

    Update jest to not rely on root babel config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts?|js?)$'
 module.exports = {
 	testRegex: TEST_REGEX,
 	transform: {
-		'^.+\\.[tj]sx?$': 'babel-jest',
+		'^.+\\.[tj]sx?$': ['babel-jest', { presets: ['next/babel'] }],
 	},
 	transformIgnorePatterns: [
 		'<rootDir>/node_modules/(?!lodash-es)',
