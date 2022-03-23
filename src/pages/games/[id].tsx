a6de64d22206f100292e6b8bdbb6954ffcd2c47c
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-03-23 22:25:23 +0100

    Move config from getStaticProps to use server config instead

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { getPlaiceholder } from 'plaiceholder'
 
-import { config } from 'config.client'
+import { config } from 'config.server'
 
 import { useGame } from 'states/games'
 
@@ -29,7 +29,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps<State> = async ({ params }) => {
-	const game = await fetcher<Game>(`/games/${params?.id as string}`, { absoluteUrl: config.staticHost })
+	const game = await fetcher<Game | undefined>(`/games/${params?.id as string}`, { absoluteUrl: config.staticHost })
+	if (!game) return { notFound: true }
 	const [{ prices }, { img: coverProps, base64: coverBlurUrl }, { img: screenshotProps, base64: screenshotBlurUrl }, ...similarGames] = await Promise.all([
 		await fetcher<{ prices: Array<GamePrice> }>(`/games/${game.id}/prices?name=${encodeURIComponent(game.name)}`, { absoluteUrl: config.staticHost }),
 		getPlaiceholder(game.cover ?? ''),
