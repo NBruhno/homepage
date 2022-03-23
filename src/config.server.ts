4376c214903c9f06b776a9af7185a6c4d3c6cf2f
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-03-23 22:17:28 +0100

    Revised STATIC_HOST env

--- src/config.server.ts
+++ src/config.server.ts
@@ -25,4 +25,5 @@ export const config = Object.freeze({
 	},
 
 	environment: process.env.NODE_ENV === 'test' ? 'development' : process.env.NODE_ENV as Environment,
+	staticHost: process.env.STATIC_HOST ?? process.env.VERCEL_ENV === 'development' ? 'http://localhost:3000' : 'https://bruhno.com' as string,
 } as const)
