ddcef86fab989def3e27390b36ba0a263caccc1d
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-05-29 15:52:48 +0200

    Fixed transparency issue for footer on landing page

--- src/containers/app/Footer/Container.tsx
+++ src/containers/app/Footer/Container.tsx
@@ -1,25 +1,33 @@
 import type { ComponentPropsWithoutRef } from 'react'
 
+import { useTheme } from '@emotion/react'
+
 type Props = ComponentPropsWithoutRef<'footer'> & {
 	isTransparent: boolean,
 }
 
-export const Container = ({ isTransparent, ...rest }: Props) => (
-	<footer
-		css={(theme) => ({
-			backgroundColor: isTransparent ? 'transparent' : theme.color.background,
-			borderTop: isTransparent ? 'none' : `1px solid ${theme.color.border}`,
-			bottom: 0,
-			color: isTransparent ? theme.color.white : theme.color.text,
-			height: '72px',
-			marginTop: 'auto',
-			padding: '12px 24px',
-			verticalAlign: 'bottom',
+export const Container = ({ isTransparent, ...rest }: Props) => {
+	const theme = useTheme()
 
-			[theme.mediaQueries.maxMobile]: {
-				textAlign: 'center',
-			},
-		})}
-		{...rest}
-	/>
-)
+	return (
+		<footer
+			style={{
+				backgroundColor: isTransparent ? 'transparent' : theme.color.background,
+			}}
+			css={(theme) => ({
+				borderTop: isTransparent ? 'none' : `1px solid ${theme.color.border}`,
+				bottom: 0,
+				color: isTransparent ? theme.color.white : theme.color.text,
+				height: '72px',
+				marginTop: 'auto',
+				padding: '12px 24px',
+				verticalAlign: 'bottom',
+
+				[theme.mediaQueries.maxMobile]: {
+					textAlign: 'center',
+				},
+			})}
+			{...rest}
+		/>
+	)
+}
--- src/containers/app/Footer/Link.tsx
+++ src/containers/app/Footer/Link.tsx
@@ -1,25 +1,33 @@
 import type { ComponentPropsWithoutRef } from 'react'
 
+import { useTheme } from '@emotion/react'
+
 type Props = ComponentPropsWithoutRef<'a'> & {
 	isTransparent: boolean,
 }
 
-export const Link = ({ isTransparent, children, ...rest }: Props) => (
-	<a
-		css={(theme) => ({
-			display: 'flex',
-			color: isTransparent ? theme.color.white : theme.color.text,
-			opacity: 0.7,
-			transition: `opacity 200ms ${theme.animation.default}`,
+export const Link = ({ isTransparent, children, ...rest }: Props) => {
+	const theme = useTheme()
 
-			'&:hover': {
-				opacity: 1,
-			},
-		})}
-		target='_blank'
-		rel='noreferrer noopener'
-		{...rest}
-	>
-		{children}
-	</a>
-)
+	return (
+		<a
+			style={{
+				color: isTransparent ? theme.color.white : theme.color.text,
+			}}
+			css={(theme) => ({
+				display: 'flex',
+				opacity: 0.7,
+				transition: `opacity 200ms ${theme.animation.default}`,
+
+				'&:hover': {
+					opacity: 1,
+				},
+			})}
+			target='_blank'
+			rel='noreferrer noopener'
+			{...rest}
+		>
+			{children}
+		</a>
+	)
+}
