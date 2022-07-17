82195ff34754ceb74b993971da392a342f3fbe09
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-07-17 22:51:11 +0200

    Link to the right resource for ladle in components route

--- src/pages/components.route.tsx
+++ src/pages/components.route.tsx
@@ -13,7 +13,7 @@ const Components: NextPage = () => {
 		<Page>
 			<iframe
 				title='ladle'
-				src={config.environment === 'development' ? `http://localhost:3001?theme=${globalTheme}` : `/ladle?theme=${globalTheme}`}
+				src={config.environment === 'development' ? `http://localhost:3001?theme=${globalTheme}` : `/ladle/index.html?theme=${globalTheme}`}
 				style={{ inset: 0, height: '100%', width: '100%', position: 'absolute' }}
 				frameBorder='0'
 				allowFullScreen
