02a472fa8431e4d3eaccee31b1606a518d94543a
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2022-09-17 22:54:09 +0200

    Cleaned up in some of the monitoring and configured the sampling rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,8 +5,9 @@ const SENTRY_DSN = process.env.SENTRY_DSN ?? process.env.NEXT_PUBLIC_SENTRY_DSN
 Sentry.init({
 	dsn: SENTRY_DSN as string,
 	tracesSampler: (context) => {
-		if (context.transactionContext.op === 'pageload') return 0.2
-		return 0.5
+		if (context.parentSampled) return true
+		if (context.transactionContext.op === 'pageload' || context.transactionContext.op === 'navigation') return 0.2
+		return 0.3
 	},
 	environment: process.env.VERCEL_ENV as string,
 })
