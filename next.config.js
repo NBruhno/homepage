f4727f6c15acbef2d6c3ecb08454d8e99a77c0dc
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2023-03-17 21:05:15 +0100

    Attempt at ignoring node_modules

--- next.config.js
+++ next.config.js
@@ -62,9 +62,9 @@ const nextConfig = {
 	experimental: {
 		// fallbackNodePolyfills: false,
 		esmExternals: true,
-		outputFileTracingIgnores: [
-			'node_modules/**/*',
-		],
+		outputFileTracingExcludes: {
+			'/api/*': ['node_modules/**/*'],
+		},
 	},
 
 	images: {
