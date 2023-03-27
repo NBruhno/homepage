7a9ea6da2032505500ac861bec526ca63f6d8be0
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2023-03-27 17:38:33 +0200

    Updated scripts for handling the new env variables

--- scripts/createEnv.js
+++ scripts/createEnv.js
@@ -10,36 +10,41 @@ const { VERCEL_ENV, IGDB_CLIENT_ID, IGDB_CLIENT_SECRET, VGINSIGHTS_EMAIL, VGINSI
 const createEnv = async () => {
 	if (VERCEL_ENV && VERCEL_ENV !== 'development') {
 		try {
-			const igdbToken = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${IGDB_CLIENT_ID}&client_secret=${IGDB_CLIENT_SECRET}&grant_type=client_credentials`, {
-				method: 'POST',
-			}).then(async (response) => {
-				const responseBody = await response.json()
-				return responseBody.access_token
-			})
+			await Promise.all([
+				(async () => {
+					const igdbToken = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${IGDB_CLIENT_ID}&client_secret=${IGDB_CLIENT_SECRET}&grant_type=client_credentials`, {
+						method: 'POST',
+					}).then(async (response) => {
+						const responseBody = await response.json()
+						return responseBody.access_token
+					})
 
-			fs.appendFile('.env', `IGDB_TOKEN="${igdbToken}"`, (error) => {
-				if (error) throw error
-				console.log('IGDB token appended to .env')
-			})
+					fs.appendFile('.env', `IGDB_TOKEN="${igdbToken}"\n`, (error) => {
+						if (error) throw error
+						console.log('IGDB token appended to .env')
+					})
+				})(),
+				(async () => {
+					const vgInsightsToken = await fetch(`https://vginsights.com/api/v1/auth/email/login`, {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify({
+							email: VGINSIGHTS_EMAIL,
+							password: VGINSIGHTS_PASSWORD,
+						}),
+					}).then(async (response) => {
+						const responseBody = await response.json()
+						return responseBody.token
+					})
 
-			const vgInsightsToken = await fetch(`https://vginsights.com/api/v1/auth/email/login`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					email: VGINSIGHTS_EMAIL,
-					password: VGINSIGHTS_PASSWORD,
-				}),
-			}).then(async (response) => {
-				const responseBody = await response.json()
-				return responseBody.token
-			})
-
-			fs.appendFile('.env', `VGINSIGHTS_TOKEN="${vgInsightsToken}"`, (error) => {
-				if (error) throw error
-				console.log('VG Insights token appended to .env')
-			})
+					fs.appendFile('.env', `VGINSIGHTS_TOKEN="${vgInsightsToken}"\n`, (error) => {
+						if (error) throw error
+						console.log('VG Insights token appended to .env')
+					})
+				})(),
+			])
 		} catch (error) {
 			console.log(error)
 			throw new Error('Failed to generate deploy env')
