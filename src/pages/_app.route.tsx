30809c7846353658ce74756f3ceafdff34fb10dc
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2024-03-14 20:08:06 +0100

    Added SpeedInsights from vercel

--- src/pages/_app.route.tsx
+++ src/pages/_app.route.tsx
@@ -3,6 +3,7 @@
 import type { AppProps } from 'next/app'
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import Head from 'next/head'
 
 import { usePage } from 'states/page'
@@ -22,6 +23,7 @@ const MyApp = ({ Component, pageProps, err }: Props) => {
 		<>
 			<PortalTarget />
 			<Analytics />
+			<SpeedInsights />
 			<Head>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta name='sentry-trace' content='{{ span.toSentryTrace() }}' />
