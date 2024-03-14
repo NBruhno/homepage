99abcb04c9a0f99609e98273364e17494292acc5
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2024-03-14 21:35:09 +0100

    Game page now tries to find the best video to autoplay

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import YouTubeEmbed from 'react-lite-youtube-embed'
 
 import { Classes } from './Classes'
@@ -5,17 +6,35 @@ import { Classes } from './Classes'
 type Props = {
 	id: string,
 	name: string,
+	shouldAutoplay?: boolean,
+	isMuted?: boolean,
 }
 
-export const Video = ({ id, name }: Props) => (
-	<>
-		<Classes />
-		<YouTubeEmbed
-			id={id}
-			title={name}
-			adNetwork={false}
-			poster='maxresdefault'
-			noCookie
-		/>
-	</>
-)
+export const Video = ({ id, name, shouldAutoplay = true, isMuted = true }: Props) => {
+	const containerRef = useRef<HTMLDivElement | null>(null)
+	const [hasBeenClicked, setHasBeenClicked] = useState(false)
+
+	useEffect(() => {
+		if (containerRef.current && shouldAutoplay && !hasBeenClicked) {
+			const [videoWrapperElement] = containerRef.current.getElementsByTagName('figure')
+			videoWrapperElement.click()
+			setHasBeenClicked(true)
+		}
+	}, [containerRef, shouldAutoplay, hasBeenClicked])
+
+	return (
+		<div ref={containerRef}>
+			<Classes />
+			<YouTubeEmbed
+				id={id}
+				title={name}
+				adNetwork={false}
+				poster='maxresdefault'
+				playlist={false}
+				containerElement='figure'
+				noCookie
+				muted={isMuted}
+			/>
+		</div>
+	)
+}
