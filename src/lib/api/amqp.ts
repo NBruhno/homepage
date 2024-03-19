012728c378eb08c696e68d2354b215c39b4a5550
Nicolai Bruhn Lauritsen <mail@bruhno.com>
2024-03-19 09:43:14 +0100

    Use separate channels for each queue for amqp

--- src/lib/api/amqp.ts
+++ src/lib/api/amqp.ts
@@ -5,7 +5,6 @@ import { config } from 'config.server'
 export const createAmqp = async () => {
 	const client = new AMQPClient(config.amqp.url)
 	await client.connect()
-	const channel = await client.channel()
 
-	return { channel, client }
+	return client
 }
